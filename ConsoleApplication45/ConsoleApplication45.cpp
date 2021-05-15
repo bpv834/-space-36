31baaf87ad0444d6cdfe6cc5d9320cee3d084fd2
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-16 02:25:14 +0900

    초기 커밋.

---
+++ ConsoleApplication45/ConsoleApplication45.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+
+	int cot(const char* s) {
+		int count = 0;
+		while (s[0] != '\0') {
+			if (isspace(s[0]))
+				count++;
+				s++;
+		}
+		return count;
+	}
+
+	int main() {
+		char str[64] = "this program/ttests const pointer to string.\n";
+
+		puts(str);
+		printf("공백 문자의 개수:%d", cot(str));
+	
+	}
